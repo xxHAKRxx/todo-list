b2f2234a77bcd974070d060e1603f8ca9370ad53
EvanMW <evanmwestcomb2006@gmail.com>
2026-01-29 10:26:09 -0500

    Create Django project and app. Add a Todo model with the attributes name and complete_by. Register the Todo model in admin.py

---
+++ todos/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Todo(models.Model):
+    name = models.CharField(max_length=150)
+    complete_by = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
